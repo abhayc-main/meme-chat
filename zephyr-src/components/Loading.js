45783aecd4eab6cadc358f162316c78126197141
abhayc-glitch <abhichbeium@gmail.com>
2021-07-02 15:51:40 -0700

    Firebase Config

--- zephyr-src/components/Loading.js
+++ zephyr-src/components/Loading.js
@@ -6,6 +6,7 @@ function Loading() {
     return (
         <center style ={{ display: "grid", placeItems: "center", height : "100vh" }}>
             <div>
+                <img src= "/favicon2.ico"></img>
                 <strong>The Zephyr</strong>
                 <Wave color="#7A63FF" size = {60} />
             </div>
