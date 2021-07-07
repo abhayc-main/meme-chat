319711d305ecc0fb3d4fb95bcf6ae4c41effff88
abhayc-glitch <abhichbeium@gmail.com>
2021-07-07 10:25:41 -0700

    Finshed Up SSR with NEXT.js

--- zephyr-src/pages/chat/[id].js
+++ zephyr-src/pages/chat/[id].js
@@ -1,16 +1,23 @@
 import styled from "styled-components"
 import Head from "next/head"
+import { useAuthState} from "react-firebase-hooks/auth"
 import Sidebar from "../../components/Sidebar"
+import { db } from "../../firebase"
+import { auth } from "../firebase"
 
-function Chat() {
+
+function Chat({chat, messages}) {
+
+    const [user] = useAuthState(auth)
+
     return (
         <Container>
             <Head>
-                <title>Chat</title>
+                <title>{getRecipientEmail(chat.users, user)}</title>
             </Head>
             <Sidebar />
             <ChatContainer>
-                <ChatScreen />
+                <ChatScreen chat = {chat} messages = {messages}/>
             </ChatContainer>
         </Container>
     )
@@ -21,7 +28,34 @@ export default Chat
 export async function getSeverSideProps(context){
     const ref = db.collections("chats").doc(context.query.id)
     // Prep Message 
-    const messagesRef = await ref.collection('messages').orderBy('timestamp','asc').get()
+    const messagesRef = await ref
+    .collection('messages')
+    .orderBy('timestamp','asc')
+    .get()
+
+    const messages = messagesRef.doc.map(doc =>({
+        id: doc.id,
+        ...doc.data(),
+    })).map(messages => ({
+        ...messages,
+        timestamp: messages.timestamp.toDate().getTime()
+    }))
+
+    //Prepping the Chat
+    const chatRes = await ref.get()
+    const chat = {
+        id: chatRes.id,
+        ...chatRes.data()
+    }
+
+    return{
+        props: {
+            //Stringfying because the messages has to be a string
+            // You cant pass an object
+            messages: JSON.stringify(messages),
+            chat: chat,
+        }
+    }
 }
 
 const Container = styled.div`
